be586d5cb9aa7210539ee68c21d7794d5d0f8912
Alejandro Uribe <rulitosxd1@gmail.com>
2020-02-13 18:06:24 -0500

    Added functions to the game

--- Godot/HangedMan/HangedMan.gd
+++ Godot/HangedMan/HangedMan.gd
@@ -1,13 +1,16 @@
 extends Control
 
-var word = "physics"
+var word = "physics h"
 var guess = ""
 
 var numTries = 9
 
 func numLines(word):
 	for i in word:
-		guess = guess + "_"
+		if(i != " "):
+			guess = guess + "_"
+		else:
+			guess = guess + " "
 	return guess
 
 func _on_inputBox_text_entered(text):
@@ -17,8 +20,11 @@ func _on_inputBox_text_entered(text):
 			if(word[i] == text):
 				guess[i] = text
 		get_node("guessWord").text = guess
+		print(get_node("guessWord").text)
+		verify_win(word)
 	else:
-		print("False")
+		numTries = verify_lose(numTries)
+		get_node("numTries").text = "Intentos restantes:" + str(numTries)
 	get_node("inputBox").clear()
 
 
@@ -26,7 +32,29 @@ func _on_inputBox_text_entered(text):
 func _ready():
 	guess = numLines(word);
 	get_node("guessWord").text = guess
+	get_node("numTries").text = "Intentos restantes: 9" 
 	print(guess)
 
+#Cambiar por un for para que detecte los espacios y los quite
+func verify_win(word):
+	if(word == get_node("guessWord").text):
+		var text = "You win"
+		GameOver(text)
+
+#Verifica cuando el jugador va perdiendo turnos
+func verify_lose(numTries):
+	var value = false
+	numTries = numTries-1
+	if(numTries == 0):
+		print("Pailas")
+	return numTries
 
 
+
+func GameOver(text):
+	get_node("inputBox").queue_free()
+	get_node("GameStatus").text = text
+
+# Función que me hace reset del programa
+func _on_TextureButton_pressed():
+	get_tree().reload_current_scene()
