0b7c782cdea336bd71d576378222eb89e9cb9909
CamiloGomezV <gomezvargas38@gmail.com>
2020-02-20 18:33:38 -0500

    a lot of changes

--- Godot/HangedMan/HangedMan.gd
+++ Godot/HangedMan/HangedMan.gd
@@ -2,8 +2,30 @@ extends Control
 
 var word = "physics h"
 var guess = ""
+var Tries = 9
 
-var numTries = 9
+onready var guessWord = get_node("VBoxContainer/guessWord")
+onready var numTries = get_node("VBoxContainer/HBoxContainer2/numTries")
+onready var ResetLabel = get_node("VBoxContainer/HBoxContainer2/ResetLabel")
+onready var inputBox = get_node("VBoxContainer/HBoxContainer2/inputBox")
+onready var Area2ds = get_node("VBoxContainer/Area2D/Sprite")
+onready var GameStatus = get_node("VBoxContainer/GameStatus")
+
+var stage = [
+	preload("res://assets/assets/stage0.png"),
+	preload("res://assets/assets/stage1.png"),
+	preload("res://assets/assets/stage2.png"),
+	preload("res://assets/assets/stage3.png"),
+	preload("res://assets/assets/stage4.png"),
+	preload("res://assets/assets/stage5.png"),
+	preload("res://assets/assets/stage6.png"),
+	preload("res://assets/assets/stage7.png"),
+	preload("res://assets/assets/stage8.png"),
+	preload("res://assets/assets/stage9.png")
+	]
+
+
+
 
 func numLines(word):
 	for i in word:
@@ -19,46 +41,46 @@ func _on_inputBox_text_entered(text):
 		for i in range(0,word.length()):
 			if(word[i] == text):
 				guess[i] = text
-		get_node("guessWord").text = guess
-		print(get_node("guessWord").text)
+		guessWord.text = guess
+		print(str(guessWord.text))
 		verify_win(word)
 	else:
-		numTries = verify_lose(numTries)
-		get_node("numTries").text = "Intentos restantes:" + str(numTries)
-	get_node("inputBox").clear()
+		Tries = verify_lose(Tries)
+		numTries.text = "Intentos restantes:" + str(Tries)
+	inputBox.clear()
 
 
 
 func _ready():
 	guess = numLines(word);
-	get_node("guessWord").text = guess
-	get_node("numTries").text = "Intentos restantes: 9"
-	get_node("ResetLabel").text = "Reset"
+	guessWord.text = guess
+	numTries.text = "Intentos restantes: 9"
+	ResetLabel.text = "Reset"
 	print(guess)
 
 # Verifica cuando el usuario gana
 func verify_win(word):
-	if(word == get_node("guessWord").text):
+	if(word == guessWord.text):
 		var text = "You win"
 		GameOver(text)
 
 #Verifica cuando el jugador va perdiendo turnos
-func verify_lose(numTries):
-	numTries = numTries-1
-	var num = 9-numTries
+func verify_lose(Tries):
+	Tries = Tries-1
+	var num = 9-Tries
 	var strpath = "res://assets/assets/stage"+str(num)+".png"
 	var path = load(strpath)
-	var Draw = get_node("Area2D/Sprite")
+	var Draw = Area2ds
 	Draw.set_texture(path)
-	if(numTries == 0):
+	if(Tries == 0):
 		GameOver("You loose")
-	return numTries
+	return Tries
 
 
 
 func GameOver(text):
-	get_node("inputBox").queue_free()
-	get_node("GameStatus").text = text
+	inputBox.queue_free()
+	GameStatus.text = text
 
 
 # Función que me hace reset del programa
