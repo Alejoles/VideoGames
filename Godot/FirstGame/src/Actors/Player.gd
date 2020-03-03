dfd802a4617186caa617ff89e3a7952765c0dd2a
Alejo <rulitosxd1@gmail.com>
2020-03-02 23:10:46 -0500

    up and down attacks

--- Godot/FirstGame/src/Actors/Player.gd
+++ Godot/FirstGame/src/Actors/Player.gd
@@ -2,7 +2,8 @@ extends Actor
 
 
 func _ready():
-	get_node("Sword/CollisionShape2D").set_disabled(true)
+	get_node("Sword_x/CollisionShape2D").set_disabled(true)
+	get_node("Sword_y/CollisionShape2D").set_disabled(true)
 
 
 func _on_EnemyDetector_body_entered(body):
@@ -14,21 +15,45 @@ func _physics_process(delta: float)->void:
 	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-	if(_velocity.x > 0):
-		get_node("Sword").scale.x = 1
-	elif(_velocity.x < 0):
-		get_node("Sword").scale.x = -1
-	if Input.get_action_strength("attack") == 1:
-		get_node("Sword/CollisionShape2D").set_disabled(false)
-	else:
-		get_node("Sword/CollisionShape2D").set_disabled(true)
+	hit_direction(direction)
+	
 
+
+func get_hit() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("attack_right") - Input.get_action_strength("attack_left"),
+		Input.get_action_strength("attack_down") - Input.get_action_strength("attack_up")
+	)
+
+
+func enable_hit():
+	if Input.get_action_strength("general_attack_x") == 1:
+		get_node("Sword_x/CollisionShape2D").set_disabled(false)
+	else:
+		get_node("Sword_x/CollisionShape2D").set_disabled(true)
+	if Input.get_action_strength("general_attack_y") == 1:
+		get_node("Sword_y/CollisionShape2D").set_disabled(false)
+	else:
+		get_node("Sword_y/CollisionShape2D").set_disabled(true)
+
+
+func hit_direction(direction: Vector2):
+	if get_hit().x == 1:
+		get_node("Sword_x").scale.x = 1
+	elif get_hit().x == -1:
+		get_node("Sword_x").scale.x = -1
+	if get_hit().y == 1:
+		get_node("Sword_y").scale.y = -1
+	elif get_hit().y == -1:
+		get_node("Sword_y").scale.y = 1
+	enable_hit()
 
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 	)
+
 
 func calculate_move_velocity(
 		linear_velocity: Vector2,
